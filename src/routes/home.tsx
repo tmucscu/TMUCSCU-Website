bacd4d5a657509a7eaeff6d2e7a6ef1483fc6ff2
Jason Truong <jason.truong@ryerson.ca>
2024-05-26 16:01:33 -0400

    create home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,10 +1,23 @@
+import Logo from "../assets/csculogo.png";
 import Navbar from "../navbar";
 
 const Home = () => {
   return (
-    <div>
+    <div className="h-screen">
       <Navbar />
-      <p>Hello World</p>
+      <div className="mx-[7%] h-[50%] items-center grid grid-cols-5">
+        <div className="col-span-3">
+          <h2 className="no-underline">Toronto Metropolitan University's</h2>
+          <h1>Computer Science Course Union</h1>
+          <p className="pt-2">
+            For over 20 years, the CSCU has been the organization representing
+            all the undergraduate Computer Science students at TMU.
+          </p>
+        </div>
+        <div className="col-span-2 flex justify-center">
+          <img className="w-[250px]" src={Logo} />
+        </div>
+      </div>
     </div>
   );
 };
