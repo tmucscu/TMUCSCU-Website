6bf66bedd9d040489830bd5848b6e8e8606700f7
Jason Truong <jason.truong@ryerson.ca>
2024-05-26 16:37:29 -0400

    create textpage component to reuse for pages with long text

---
+++ src/TextPage.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./navbar";
+import { ReactNode } from "react";
+
+const TextPage = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="h-screen">
+      <Navbar />
+      <div className="mx-[7%] items-center grid grid-cols-5">
+        <div className="col-span-3">{children}</div>
+        <div className="col-span-2 flex justify-center">
+          Image carousel here lol
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TextPage;
