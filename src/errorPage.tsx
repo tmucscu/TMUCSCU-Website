ef638cc0c0be4ebd2fcbeb05be0e185621cd3a29
Jason Truong <jason.truong@ryerson.ca>
2024-05-26 16:01:23 -0400

    create error page for routing errors

---
+++ src/errorPage.tsx
@@ -0,0 +1,32 @@
+import Navbar from "./navbar";
+import { useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  type error = {
+    status?: number;
+    statusText?: string;
+    message?: string;
+  };
+  const error = useRouteError() as error;
+  console.error(error);
+
+  return (
+    <div className="h-screen">
+      <Navbar />
+      <div className="mx-[7%] h-[50%] items-center grid grid-cols-5">
+        <div className="col-span-3">
+          <h1>Uh Oh!</h1>
+          <h2 className="no-underline pb-3">
+            Sorry, we can't find the page you're looking for
+          </h2>
+          <p>{`Error ${error.status}: ${error.statusText || error.message}`}</p>
+        </div>
+        <div className="col-span-2 flex justify-center">
+          <h1 className="text-9xl">?</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import "./index.css";
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import ErrorPage from "./errorPage.tsx";
 import Home from "./routes/home.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home />, errorElement: <ErrorPage /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
