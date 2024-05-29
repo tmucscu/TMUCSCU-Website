51336e7b839fc6940f1c43abb799840a9ea45135
Jason Truong <jason.truong@ryerson.ca>
2024-05-29 02:20:16 -0400

    set up api for getting carousel images

--- components/carousel/imageCarousel.tsx
+++ components/carousel/imageCarousel.tsx
@@ -5,92 +5,47 @@ import { useEffect, useState } from "react";
 import Slider from "react-slick";
 import clsx from "clsx";
 
-// import axios from "axios";
-
-// TODO: Clean up this mess
 // TODO: useContext so state is maintained across pages (carousel shouldn't reset)
 const ImageCarousel = () => {
-  // type Carousel = {
-  //   images: string[];
-  //   ttl: string;
-  // };
+  type Carousel = {
+    images: string[];
+    ttl: string;
+  };
 
   const [carouselImages, setCarouselImages] = useState<string[]>([]);
-  // const FOLDER_ID = "";
-
-  // const getImageIdsFromGFolder = (folderId: string) => {
-  //   const apiKey = "";
-  //   const url = "https://www.googleapis.com/drive/v3/files";
-  //   const params = {
-  //     q: `'${folderId}' in parents and trashed=false`,
-  //     key: apiKey,
-  //     fields: "files(id)",
-  //     supportsAllDrives: true,
-  //     includeItemsFromAllDrives: true,
-  //   };
-  //   return axios
-  //     .get(url, { params })
-  //     .then((response) => {
-  //       if (response.status === 200) {
-  //         const files = response.data.files;
-  //         const imageIds: string[] = [];
-  //         files.forEach((file: { id: string }) => {
-  //           imageIds.push(file.id);
-  //         });
-
-  //         return imageIds;
-  //       } else {
-  //         console.error(
-  //           `Error: ${response.status} - ${response.data.error.message}`
-  //         );
-  //         return [];
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error(`Error: ${error.message}`);
-  //       return [];
-  //     });
-  // };
-
   useEffect(() => {
-    setCarouselImages([
-      "1tcBmYpIY71vW_PfWm8KcQGXttftmFu_b",
-      "1crQUvj8OmRm6lXappwgAy1_-r5YrXq11",
-      "1YUj0tQqlUVq6RS7AAukRznnhl3IRLxNe",
-      "1GJp4NERydWUr4nwAim4c-F7bO9ipi_VE",
-      "1PBq-SrB1BH_OEHQVEfoJFPqLPv4W0RnK",
-      "1pj__8TI1dyNjU5OMBpjZsqSxZNObnM4P",
-      "1owBzu8644F6XhQfeE8cYWURZ0hS6YLXa",
-      "1tmNHD-EnD3Y22KLku7GDln3FgknbhjZq",
-      "1nlhZ1H7UHcQu38ZK2s1njXZs7v1twp6O",
-      "1ZZ8eIaJUoOdN_yKxK6P4CyjWQEMck1nh",
-    ]);
-    // const TWELVE_HOURS_IN_MS = 12 * 60 * 60 * 1000;
+    const TWELVE_HOURS_IN_MS = 12 * 60 * 60 * 1000;
 
-    // const carouselCache = localStorage.getItem("carousel");
-    // const carousel: Carousel | null = carouselCache
-    //   ? JSON.parse(carouselCache)
-    //   : null;
-    // const now = new Date().getTime();
+    const getImageIdsForCarousel = async () => {
+      const carouselCache = localStorage.getItem("carousel");
 
-    // const shouldGetImages =
-    //   !carousel ||
-    //   (carousel &&
-    //     (carousel.images.length === 0 ||
-    //       now - Number(carousel?.ttl) >= TWELVE_HOURS_IN_MS));
+      const carousel: Carousel | null = carouselCache
+        ? JSON.parse(carouselCache)
+        : null;
 
-    // if (shouldGetImages) {
-    //   getImageIdsFromGFolder(FOLDER_ID).then((fileIds) => {
-    //     console.log(fileIds);
-    //     localStorage.setItem(
-    //       "carousel",
-    //       JSON.stringify({ images: fileIds, ttl: now.toString() })
-    //     );
-    //     setCarouselImages(fileIds);
-    //   });
-    // } else if (carousel) {
-    //   setCarouselImages(carousel.images);
-    // }
+      const now = new Date().getTime();
+
+      // fetch images if no carousel in local storage or images are empty or ttl expired
+      const shouldFetchImages =
+        !carousel ||
+        (carousel &&
+          (carousel.images.length === 0 ||
+            now - Number(carousel?.ttl) >= TWELVE_HOURS_IN_MS));
+
+      if (shouldFetchImages) {
+        const response = await fetch("/api/carousel");
+        const { imageIds } = await response.json();
+        localStorage.setItem(
+          "carousel",
+          JSON.stringify({ images: imageIds, ttl: now.toString() })
+        );
+        setCarouselImages(imageIds);
+      } else if (carousel) {
+        setCarouselImages(carousel.images);
+      }
+    };
+
+    getImageIdsForCarousel();
   }, []);
 
   const settings = {
---
+++ pages/api/carousel.ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import axios from 'axios';
+
+type Data = {
+    imageIds: string[];
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  return new Promise<void>((resolve, reject) => {
+    getImageIdsFromGFolder(process.env.GDRIVE_FOLDER_ID).then((imageIds) => {
+      res.status(200).json({ imageIds: imageIds });
+      resolve();
+    }).catch((error) => {
+      console.error(`ERROR: ${error}`)
+      res.status(400).json({ imageIds: [] });
+      resolve();
+    })
+  });
+}
+
+const getImageIdsFromGFolder = (folderId: string) => {
+    const apiKey = process.env.GDRIVE_API_KEY;
+    const url = "https://www.googleapis.com/drive/v3/files";
+    const params = {
+      q: `'${folderId}' in parents and trashed=false`,
+      key: apiKey,
+      fields: "files(id)",
+      supportsAllDrives: true,
+      includeItemsFromAllDrives: true,
+    };
+    return axios
+      .get(url, { params })
+      .then((response) => {
+        if (response.status === 200) {
+          const files = response.data.files;
+          const imageIds: string[] = [];
+          files.forEach((file: { id: string }) => {
+            imageIds.push(file.id);
+          });
+
+          return imageIds;
+        } else {
+          console.error(
+            `Error: ${response.status} - ${response.data.error.message}`
+          );
+          return [];
+        }
+      })
+      .catch((error) => {
+        console.error(`Error: ${error.message}`);
+        return [];
+      });
+  };
