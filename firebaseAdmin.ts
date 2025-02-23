dfed64e23bc790caad3d17ae01c02703dd5c8281
Jason Truong <jason.truong@ryerson.ca>
2025-02-23 17:46:32 -0500

    add extra slash

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -5,7 +5,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\n/g, "\n") : undefined
+      privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined
     }),
   });
 } else {
