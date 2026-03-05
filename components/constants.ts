204c83709da15fd8613043d277c278f233f813c1
Jennifer Chung <jennifer.chung14@gmail.com>
2026-03-05 00:27:35 -0500

    updated meeting min link to 2025-2026 year

--- components/constants.ts
+++ components/constants.ts
@@ -4,7 +4,7 @@ export const Routes = {
     ABOUT: "/about",
     TEAM: "/team",
     EVENTS: "/events",
-    MEETING_MINUTES: "https://drive.google.com/drive/folders/1dDwBoiifrl8SLrFytE-Se27NYegyOJsL?usp=sharing",
+    MEETING_MINUTES: "https://drive.google.com/drive/folders/1ccHhISzy9Gjziu7SQYyME1Fzl0gOSNfm?usp=sharing",
     CONTACT: "/contact",
     WIKI: "https://wiki.tmucscu.com"
 }
